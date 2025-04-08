2f7550a7f34b2debe8f04f349d23ecbd7567c9a4
hyukjunlim <dlagurwns03@gmail.com>
2025-04-08 23:33:03 +0900

    Add files via upload

--- run_slurm.sh
+++ run_slurm.sh
@@ -2,30 +2,30 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=2      # Cores per node
 #SBATCH --partition=snu-gpu1     # Partition Name
-#SBATCH --job-name=flow     # Default job name
+#SBATCH --job-name=mpflow     # Default job name
 #SBATCH --time=10-00:00:00          # Runtime: 10 days
 #SBATCH -o output/%x.%N.%j.out   # STDOUT with job name in output directory
-#SBATCH -e /dev/null   # Discard STDERR (no output file)
-#SBATCH --gres=gpu:1       # Request 1 A5000 GPU
+#SBATCH -e output/%x.%N.%j.err   # STDERR with job name in output directory
+#SBATCH --gres=gpu:a5000:1       # Request 1 A5000 GPU
 
-# Check if a script was provided as an argument
-if [ $# -eq 0 ]; then
-    echo "Error: No Python script specified"
-    echo "Usage: sbatch run_slurm.sh <python_script.py> [job_name]"
-    exit 1
-fi
+# # Check if a script was provided as an argument
+# if [ $# -eq 0 ]; then
+#     echo "Error: No Python script specified"
+#     echo "Usage: sbatch run_slurm.sh <python_script.py> [job_name]"
+#     exit 1
+# fi
 
-# Get the Python script from the first argument
-PYTHON_SCRIPT=$1
+# # Get the Python script from the first argument
+# PYTHON_SCRIPT=$1
 
-# Set job name from second argument or use script name as job name
-if [ $# -ge 2 ]; then
-    # A job name was provided as the second argument
-    SLURM_JOB_NAME=$2
-else
-    # Use the script name (without .py) as the job name
-    SLURM_JOB_NAME=$(basename ${PYTHON_SCRIPT} .py)
-fi
+# # Set job name from second argument or use script name as job name
+# if [ $# -ge 2 ]; then
+#     # A job name was provided as the second argument
+#     SLURM_JOB_NAME=$2
+# else
+#     # Use the script name (without .py) as the job name
+#     SLURM_JOB_NAME=$(basename ${PYTHON_SCRIPT} .py)
+# fi
 
 # mkdir output
 mkdir -p output
@@ -44,7 +44,7 @@ StartTime=$(date +%s)
 # cd $SLURM_SUBMIT_DIR
 
 # Run the specified Python script
-python ${PYTHON_SCRIPT}
+sh scripts/train/oc20/s2ef/equiformer_v2/153M_exp.sh
 
 EndTime=$(date +%s)
 
--- scripts/train/oc20/s2ef/equiformer_v2/153M_exp.sh
+++ scripts/train/oc20/s2ef/equiformer_v2/153M_exp.sh
@@ -3,7 +3,6 @@
 #     --config-yml 'oc20/configs/s2ef/all_md/equiformer_v2/153M_exp.yml' \
 #     --run-dir 'models/oc20/s2ef/2M/equiformer_v2/N@12_L@6_M@2/bs@64_lr@2e-4_wd@1e-3_epochs@12_warmup-epochs@0.1_g@1x1' \
 #     --print-every 200 \
-#     --submit \
 #     --amp \
 #     --checkpoint 'models/oc20/s2ef/2M/equiformer_v2/N@12_L@6_M@2/bs@64_lr@2e-4_wd@1e-3_epochs@12_warmup-epochs@0.1_g@1x1/mpflow_exp2_lr5e-4_wu1.pt'
 
@@ -13,20 +12,5 @@ python main_oc20.py \
     --config-yml 'oc20/configs/s2ef/all_md/equiformer_v2/153M_exp.yml' \
     --run-dir 'models/oc20/s2ef/2M/equiformer_v2/N@12_L@6_M@2/bs@64_lr@2e-4_wd@1e-3_epochs@12_warmup-epochs@0.1_g@1x1' \
     --print-every 200 \
-    --submit \
     --amp \
     --checkpoint 'models/oc20/s2ef/2M/equiformer_v2/N@12_L@6_M@2/bs@64_lr@2e-4_wd@1e-3_epochs@12_warmup-epochs@0.1_g@1x1/eq2_153M_ec4_allmd.pt'
-
-
-# python -m torch.distributed.launch \
-#     --nproc_per_node=8 \
-#     --master_port=29500 \
-#     main_oc20.py \
-#     --mode predict \
-#     --config-yml 'oc20/configs/s2ef/all_md/equiformer_v2/153M_exp.yml' \
-#     --run-dir 'models/oc20/s2ef/2M/equiformer_v2/N@12_L@6_M@2/bs@64_lr@2e-4_wd@1e-3_epochs@12_warmup-epochs@0.1_g@1x1' \
-#     --print-every 200 \
-#     --amp \
-#     --submit \
-#     --distributed \
-#     --checkpoint 'models/oc20/s2ef/2M/equiformer_v2/N@12_L@6_M@2/bs@64_lr@2e-4_wd@1e-3_epochs@12_warmup-epochs@0.1_g@1x1/eq2_153M_ec4_allmd.pt'
